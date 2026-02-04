0934c122a6f664b36ed399348c34dd1de1ada9fc
amirali <amirali10281@gmail.com>
2026-02-05 02:40:56 +0330

    fix

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def main():
     print("Hello from minorityprompt!")
 
 
+from minority_gen.grid_search import random_search
 from minority_gen.task import sharif_task
 
 if __name__ == "__main__":
     main()
-    sharif_task(model="sd15", use_lightning=True)
+    # sharif_task(model="sd15", use_lightning=True)
+    random_search(
+        # models=["sdxl", "sdxl_lightning", "sd15"],
+        models=["sd15"],
+        n_configs=25,
+        n_samples=1,
+        seed=42,
+    )
