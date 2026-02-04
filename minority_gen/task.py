ab5f036501867634d959bc6a4603c992ff174cd5
amirali <amirali10281@gmail.com>
2026-02-05 01:33:23 +0330

    fix

--- minority_gen/task.py
+++ minority_gen/task.py
@@ -116,8 +116,9 @@ def sharif_task(
             result = generator.generate(
                 prompt=prompt,
                 # modifier=modifier,
+                modifier=None,
                 seed=42 + i + seed_plus,
-                # generate_baseline=True,
+                generate_baseline=False,
                 generate_minority=True,
             )
             clean_gpu()
